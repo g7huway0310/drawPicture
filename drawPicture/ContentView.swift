bbbea7af590934a51353ed904419fa5e30929b86
lee <g7huway0310@gmail.com>
2020-07-17 18:26:47 +0800

    Initial Commit

---
+++ drawPicture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  drawPicture
+//
+//  Created by guowei on 2020/7/17.
+//  Copyright © 2020 guowei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
